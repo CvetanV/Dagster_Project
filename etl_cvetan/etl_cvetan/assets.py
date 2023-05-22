f1d7971fbd1e31df8b3a6699ea02c760d80240a3
Cvetan Veljanovski <cvetan.veljanovski@u-blox.com>
2023-05-22 17:29:16 +0200

    push code

---
+++ etl_cvetan/etl_cvetan/assets.py
@@ -0,0 +1,28 @@
+import pandas as pd  # Add new imports to the top of `assets.py`
+import requests
+from dagster import asset, get_dagster_logger # import the `dagster` library
+
+@asset # add the asset decorator to tell Dagster this is an asset
+def topstory_ids():
+    newstories_url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+    top_new_story_ids = requests.get(newstories_url).json()[:100]
+    return top_new_story_ids
+
+
+@asset
+def topstories(topstory_ids):
+    logger = get_dagster_logger()
+
+    results = []
+    for item_id in topstory_ids:
+        item = requests.get(
+            f"https://hacker-news.firebaseio.com/v0/item/{item_id}.json"
+        ).json()
+        results.append(item)
+
+        if len(results) % 20 == 0:
+            logger.info(f"Got {len(results)} items so far.")
+
+    df = pd.DataFrame(results)
+
+    return df
